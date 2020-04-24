55ed0d29929f7040976903b0d0359b16d168a311
4L3_xD <alessandra.olea@gmail.com>
2020-04-24 17:04:32 -0300

    verify the new user

--- authentication.py
+++ authentication.py
@@ -5,33 +5,49 @@ user_db = []
 password_db = []
 
 def authentication():
+
+    again = True
+
     print('That\'s your first time here?')
-    first = True
-
-    if first == True:
-        lenght_db = int(len(user_db))
-
-        print('\nSign up! Type your user name: ')
-        user = input()
-        user_db.insert(lenght_db+1, user)
-        print('Enter your password: ')
-        password = input()
-        password_db.insert(lenght_db+1, password)
-
+    not_is_first = input()
+    if not_is_first.find == 'no':
         first = False
-    else:
-        print('\nSign in!')
-        user_name = input('Type your user name: ')
-        for userName in range(len(user_db)):
-            if user_name == user_db[userName]:
-                print('Enter your password:')
-                user_password = input()
-                if user_password != password:
-                    print('Incorrect password!')
-                else:
-                    welcome()                    
-        else:
-            print('User not found!')
+    elif not_is_first == 'yes':
+        first = True
+
+    while again == True: 
+
+        if first == True:
+            lenght_db = int(len(user_db))
+
+            print('\nSign up! Type your user name: ')
+            user = input()
+            user_db.insert(lenght_db+1, user)
+            print('Enter your password: ')
+            password = input()
+            password_db.insert(lenght_db+1, password)
+
+            first = False
+            
+        elif first == False:
+            print('\nSign in!')
+            user_name = input('Type your user name: ')
+            for userName in range(len(user_db)):
+                if user_name == user_db[userName]:
+                    print('Enter your password:')
+                    user_password = input()
+                    if user_password != password:
+                        print('Incorrect password!')
+                    else:
+                        welcome()                    
+            else:
+                print('User not found!')
+
+    print('Continue (y/N)?')
+    answer = input()
+    if answer == 'n' or 'N':
+        again = False
+
 
 def welcome():
     print('Welcome!')
