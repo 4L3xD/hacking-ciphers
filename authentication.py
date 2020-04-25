f6f4e1577c7cbdb4e64aa734c6a2404acdfda0f6
4L3_xD <alessandra.olea@gmail.com>
2020-04-25 13:10:53 -0300

    add function signIn

--- authentication.py
+++ authentication.py
@@ -2,52 +2,70 @@
 import pyperclip
 
 user_db = []
+lenght_user_db = int(len(user_db))
+
 password_db = []
+lenght_password_db = int(len(password_db))
 
 def authentication():
+    again = False
 
-    again = True
+    print('='*60 + '\n{:-^60}'.format('Authentica'))
+    print('\nHello! Do you want to joke of criptograph? Y/n')
+    joke = input()
+    if joke == 'y':
+        print('Let\'s go!\n')
+        again = True
+    else:
+        print('See you later!')
 
-    print('That\'s your first time here?')
-    not_is_first = input()
-    if not_is_first.find == 'no':
-        first = False
-    elif not_is_first == 'yes':
-        first = True
+    while again == True:
 
-    while again == True: 
+        print('That\'s your first time here? Y/n')
+        is_first = input()
+        if is_first == 'y':
+            first = True
+        elif is_first == 'n':
+            first = False
 
         if first == True:
-            lenght_db = int(len(user_db))
-
             print('\nSign up! Type your user name: ')
             user = input()
-            user_db.insert(lenght_db+1, user)
+            user_db.insert(lenght_user_db+1, user)
             print('Enter your password: ')
             password = input()
-            password_db.insert(lenght_db+1, password)
+            password_db.insert(lenght_password_db+1, password)
 
             first = False
-            
-        elif first == False:
-            print('\nSign in!')
-            user_name = input('Type your user name: ')
-            for userName in range(len(user_db)):
-                if user_name == user_db[userName]:
-                    print('Enter your password:')
-                    user_password = input()
-                    if user_password != password:
-                        print('Incorrect password!')
-                    else:
-                        welcome()                    
-            else:
-                print('User not found!')
+            signIn()
 
-    print('Continue (y/N)?')
-    answer = input()
-    if answer == 'n' or 'N':
-        again = False
+        else:
+            signIn()
 
+        print('Continue (y/N)?')
+        answer = input()
+        if answer == 'n':
+            again = False
+        elif answer =='y':
+            again = True
+
+def signIn():
+    print('\nSign in!')
+    user = input('Type your user name: ')
+    for user_name in range(len(user_db)):
+        if user == user_db[user_name]:
+            user_acess = True
+        else:
+            print('User not found!')
+    if user_acess == True:
+        print('Enter your password:')
+        password = input()
+        for user_password in range(len(password_db)):
+            if password == password_db[user_password]:
+                password_acess = True
+                welcome()                    
+        if password_acess == False:
+            print('Incorrect password!')
 
 def welcome():
     print('Welcome!')
